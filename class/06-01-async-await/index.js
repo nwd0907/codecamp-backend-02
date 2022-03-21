ff4cbebb1882aa56fe50ba572c498eaf4d3c62fa
nwd0907 <nwd0907@newbizstart.com>
2022-03-21 20:18:44 +0900

    Update sms, email

---
+++ class/06-01-async-await/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+// 비동기방식
+function fetchPost(){
+    const result = axios.get("https://koreanjson.com/posts/1")
+    console.log('======================')
+    console.log(result) // Promise { <pending> }
+    console.log('======================')
+}
+
+fetchPost() // 실행명령
+
+
+// 동기방식
+async function fetchPost2(){
+    const result = await axios.get("https://koreanjson.com/posts/1")
+    console.log('======================')
+    console.log(result) // 실제데이터
+    console.log(result.data.title) // 실제데이터
+    console.log('======================')
+}
+
+fetchPost2() // 실행명령
