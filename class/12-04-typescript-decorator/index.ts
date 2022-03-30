b1b991e6618d03668917115943aa4643a53ba08a
nwd0907 <nwd0907@newbizstart.com>
2022-03-30 23:27:33 +0900

    Update private-readonly

--- class/12-04-typescript-decorator/index.ts
+++ class/12-04-typescript-decorator/index.ts
@@ -6,38 +6,3 @@ function zzz(aaaaaa) {
 
 @zzz
 class AppController {}
-
-// public
-class Aaa {
-  constructor(public mypower) {
-    this.mypower = mypower;
-  }
-
-  ggg() {
-    console.log("안녕하세요~~");
-  }
-}
-const aaa = new Aaa(50);
-aaa.mypower = 5;
-
-// private
-class Bbb {
-  constructor(private mypower) {}
-
-  ggg() {
-    this.mypower = 10;
-    console.log("안녕하세요~~");
-  }
-}
-const bbb = new Bbb(50);
-bbb.mypower = 5;
-
-// readonly
-class Ccc {
-  constructor(readonly mypower) {}
-
-  ggg() {
-    this.mypower = 10;
-    console.log("안녕하세요~~");
-  }
-}
