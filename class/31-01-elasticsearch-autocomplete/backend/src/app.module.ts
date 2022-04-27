9ffd6da155346b88d492d4e27f0488d978678092
nwd0907 <nwd0907@newbizstart.com>
2022-04-27 16:05:08 +0900

    Update msa

---
+++ class/31-01-elasticsearch-autocomplete/backend/src/app.module.ts
@@ -0,0 +1,51 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { CacheModule, Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { BoardModule } from './apis/boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductCategoryModule } from './apis/productsCategory/productCategory.module';
+import { ProductModule } from './apis/products/product.module';
+import { UserModule } from './apis/users/user.module';
+import { AuthModule } from './apis/auth/auth.module';
+import { PointTransactionModule } from './apis/pointTransaction/pointTransaction.module';
+import { FileModule } from './apis/file/file.module';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import type { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
+
+@Module({
+  imports: [
+    AuthModule,
+    BoardModule,
+    FileModule,
+    PointTransactionModule,
+    ProductModule,
+    ProductCategoryModule,
+    UserModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+      context: ({ req, res }) => ({ req, res }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'my-database',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'mydocker02',
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      url: 'redis://my-redis:6379',
+      isGlobal: true,
+    }),
+  ],
+  // controllers: [AppController],
+  // providers: [AppService],
+})
+export class AppModule {}
