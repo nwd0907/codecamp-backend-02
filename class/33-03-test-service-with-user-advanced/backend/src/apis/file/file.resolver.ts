aed8eaeaa15c0b269ce1b71f2d0f2dc702675b2c
nwd0907 <nwd0907@newbizstart.com>
2022-04-29 11:10:20 +0900

    Update test

---
+++ class/33-03-test-service-with-user-advanced/backend/src/apis/file/file.resolver.ts
@@ -0,0 +1,18 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { FileService } from './file.service';
+import { FileUpload, GraphQLUpload } from 'graphql-upload';
+
+@Resolver()
+export class FileResolver {
+  constructor(
+    private readonly fileService: FileService, //
+  ) {}
+
+  @Mutation(() => [String])
+  uploadFile(
+    @Args({ name: 'files', type: () => [GraphQLUpload] }) files: FileUpload[],
+  ) {
+    console.log('asdf');
+    return this.fileService.upload({ files });
+  }
+}
